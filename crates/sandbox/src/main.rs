3545cf726fd5ff62888c2b1e1e99e250c292a2c1
Neil <cantorne.neil@gmail.com>
2026-01-08 16:17:57 +0800

    feat: add sandbox crate for testing

---
+++ crates/sandbox/src/main.rs
@@ -0,0 +1,10 @@
+use mayon::backend::vulkan::{VulkanBackend, VulkanBackendParams};
+
+fn main() {
+    let instance = mayon::Instance::new::<'static, VulkanBackend>(
+        VulkanBackendParams::default()
+            .with_application_name("Mayon")
+            .with_engine_name("Mayon Engine"),
+    )
+    .unwrap();
+}
