e2478cd0cb98f1ed1c699e9deb313dbdba3dda2e
Neil <cantorne.neil@gmail.com>
2026-01-09 01:16:06 +0800

    fix(c-api): regenerate C header

--- crates/c_api/include/mayon.h
+++ crates/c_api/include/mayon.h
@@ -1,20 +1,37 @@
-#pragma once
+#ifndef MAYON_API_H
+#define MAYON_API_H
+
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef struct VulkanVersion {
-    uint32_t major;
-    uint32_t minor;
-    uint32_t patch;
+  uint32_t major;
+  uint32_t minor;
+  uint32_t patch;
 } VulkanVersion;
 
 typedef struct VulkanBackendParams {
-    const char* application_name;
-    VulkanVersion application_version;
-    const char* engine_name;
-    VulkanVersion engine_version;
+  const char *application_name;
+  struct VulkanVersion application_version;
+  const char *engine_name;
+  struct VulkanVersion engine_version;
 } VulkanBackendParams;
 
-int mayon_new_instance_on_vulkan(
-    const VulkanBackendParams* params,
-    Instance* out_instance
-);
+typedef struct Instance {
+  uintptr_t _0;
+} Instance;
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int32_t mayon_new_instance_on_vulkan(const struct VulkanBackendParams *param,
+                                     struct Instance *out_instance);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  /* MAYON_API_H */
