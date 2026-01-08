ee9a82ee9dfc80e4131e711028f549af8b5b4fe2
Neil <cantorne.neil@gmail.com>
2026-01-08 21:11:37 +0800

    feat(vulkan): add VkResult

--- crates/mayon/src/backends/vulkan/types/mod.rs
+++ crates/mayon/src/backends/vulkan/types/mod.rs
@@ -2,10 +2,12 @@ mod allocation_callbacks;
 mod application_info;
 mod instance;
 mod instance_create_info;
+mod result;
 mod structure_type;
 
 pub(super) use allocation_callbacks::*;
 pub(super) use application_info::ApplicationInfo;
 pub(super) use instance::Instance;
 pub(super) use instance_create_info::InstanceCreateInfo;
+pub(super) use result::VkResult;
 pub(super) use structure_type::StructureType;
