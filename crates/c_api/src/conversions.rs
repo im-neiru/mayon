1c6cbe259290572e23ffe811222835a2ec7e768e
Neil <cantorne.neil@gmail.com>
2026-01-09 01:51:10 +0800

    fix(c-api): add conversion for instance and make converter inline

--- crates/c_api/src/conversions.rs
+++ crates/c_api/src/conversions.rs
@@ -1,11 +1,13 @@
 use core::{
     ffi::{CStr, c_char},
     mem::transmute,
+    ops::{Deref, DerefMut},
 };
 
 use crate::rs;
 
 impl From<super::VulkanVersion> for rs::VulkanVersion {
+    #[inline(always)]
     fn from(value: super::VulkanVersion) -> Self {
         let super::VulkanVersion {
             major,
@@ -22,12 +24,34 @@ impl From<super::VulkanVersion> for rs::VulkanVersion {
 }
 
 impl From<rs::Instance> for super::Instance {
+    #[inline(always)]
     fn from(value: rs::Instance) -> Self {
-        unsafe { transmute(value) }
+        unsafe { transmute::<rs::Instance, Self>(value) }
     }
 }
 
-pub(crate) fn ptr_to_op_cstr<'a>(ptr: *const c_char) -> Option<&'a CStr> {
+impl Deref for super::Instance {
+    type Target = rs::Instance;
+
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        let ptr: *const Self::Target = (self as *const Self).cast();
+
+        unsafe { ptr.as_ref().unwrap_unchecked() }
+    }
+}
+
+impl DerefMut for super::Instance {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        let ptr: *mut Self::Target = (self as *mut Self).cast();
+
+        unsafe { ptr.as_mut().unwrap_unchecked() }
+    }
+}
+
+#[inline(always)]
+pub(crate) const fn ptr_to_op_cstr<'a>(ptr: *const c_char) -> Option<&'a CStr> {
     if ptr.is_null() {
         None
     } else {
