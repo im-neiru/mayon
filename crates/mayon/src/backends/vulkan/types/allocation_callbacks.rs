e7987f1df3c3321cfd881342b6fc324b77b07783
Neil <cantorne.neil@gmail.com>
2026-01-08 20:10:58 +0800

    feat(vulkan): add AllocationCallbacks

---
+++ crates/mayon/src/backends/vulkan/types/allocation_callbacks.rs
@@ -0,0 +1,100 @@
+use core::{
+    alloc::{Allocator, Layout},
+    ffi::c_void,
+    marker::PhantomData,
+    ptr::NonNull,
+};
+
+use std::alloc::handle_alloc_error;
+
+#[repr(C)]
+pub(in crate::backends::vulkan) struct AllocationCallbacks<'a, A>
+where
+    A: Allocator,
+{
+    pub user_data: *const c_void,
+    pub fn_allocation: FnAllocationFunction,
+    pub fn_reallocation: FnReallocationFunction,
+    pub fn_free: FnFreeFunction,
+    pub fn_internal_allocation: FnInternalAllocationNotification,
+    pub fn_internal_free: FnInternalFreeNotification,
+    pub _marker: PhantomData<&'a A>,
+}
+
+impl<'a, A> AllocationCallbacks<'a, A>
+where
+    A: Allocator,
+{
+    pub(crate) fn new(allocator: NonNull<A>) -> Self {
+        Self {
+            // TODO: redirect allocations to allocator
+            user_data: allocator.as_ptr().cast(),
+            fn_allocation: None,
+            fn_reallocation: None,
+            fn_free: None,
+            fn_internal_allocation: None,
+            fn_internal_free: None,
+            _marker: Default::default(),
+        }
+    }
+}
+
+pub(in crate::backends::vulkan) type FnAllocationFunction = Option<
+    unsafe extern "system" fn(
+        user_data: *mut c_void,
+        size: usize,
+        alignment: usize,
+        allocation_scope: SystemAllocationScope,
+    ) -> *mut c_void,
+>;
+
+pub(in crate::backends::vulkan) type FnReallocationFunction = Option<
+    unsafe extern "system" fn(
+        user_data: *mut c_void,
+        original: *mut c_void,
+        size: usize,
+        alignment: usize,
+        allocation_scope: SystemAllocationScope,
+    ) -> *mut c_void,
+>;
+
+pub type FnFreeFunction =
+    Option<unsafe extern "system" fn(p_user_data: *mut c_void, p_memory: *mut c_void)>;
+
+pub(in crate::backends::vulkan) type FnInternalAllocationNotification = Option<
+    unsafe extern "system" fn(
+        user_data: *mut c_void,
+        size: usize,
+        allocation_type: InternalAllocationType,
+        allocation_scope: SystemAllocationScope,
+    ),
+>;
+
+pub(in crate::backends::vulkan) type FnInternalFreeNotification = Option<
+    unsafe extern "system" fn(
+        user_data: *mut c_void,
+        size: usize,
+        allocation_type: InternalAllocationType,
+        allocation_scope: SystemAllocationScope,
+    ),
+>;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
+pub(in crate::backends::vulkan) struct SystemAllocationScope(pub(crate) i32);
+
+impl SystemAllocationScope {
+    pub(in crate::backends::vulkan) const COMMAND: Self = Self(0);
+    pub(in crate::backends::vulkan) const OBJECT: Self = Self(1);
+    pub(in crate::backends::vulkan) const CACHE: Self = Self(2);
+    pub(in crate::backends::vulkan) const DEVICE: Self = Self(3);
+    pub(in crate::backends::vulkan) const INSTANCE: Self = Self(4);
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
+pub(in crate::backends::vulkan) struct InternalAllocationType(pub(crate) i32);
+
+impl InternalAllocationType {
+    pub(in crate::backends::vulkan) const EXECUTABLE: Self = Self(0);
+}
--- crates/mayon/src/backends/vulkan/types/mod.rs
+++ crates/mayon/src/backends/vulkan/types/mod.rs
@@ -1,7 +1,9 @@
+mod allocation_callbacks;
 mod application_info;
 mod instance_create_info;
 mod structure_type;
 
+pub(super) use allocation_callbacks::*;
 pub(super) use application_info::ApplicationInfo;
 pub(super) use instance_create_info::InstanceCreateInfo;
 pub(super) use structure_type::StructureType;
