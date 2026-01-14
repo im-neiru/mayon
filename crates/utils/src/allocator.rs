cf1885013eee1f3378ca1a2cb9e0d03cea0f1f11
Neil <cantorne.neil@gmail.com>
2026-01-14 15:41:17 +0800

    test(alloc_utils): add tests cases for AllocationUtils

--- crates/utils/src/allocator.rs
+++ crates/utils/src/allocator.rs
@@ -122,3 +122,119 @@ const fn compute_layout(data_layout: Layout) -> Result<(Layout, [usize; 2]), All
 
     Ok((block_layout, [layout_offset, data_offset]))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::alloc::Global;
+
+    #[test]
+    fn allocate_and_deallocate_simple() {
+        unsafe {
+            let alloc = Global;
+
+            let layout = Layout::from_size_align(32, 8).unwrap();
+            let mut ptr = alloc.allocate_with_stored_layout(layout).unwrap();
+
+            for (index, dest) in ptr.as_mut().iter_mut().enumerate() {
+                *dest = index as u8;
+            }
+
+            alloc.deallocate_with_stored_layout(ptr.cast());
+        }
+    }
+
+    #[test]
+    fn allocation_alignment_is_respected() {
+        unsafe {
+            let alloc = Global;
+
+            let alignments = [1, 2, 4, 8, 16, 32, 64];
+
+            for &align in &alignments {
+                let layout = Layout::from_size_align(24, align).unwrap();
+                let ptr = alloc.allocate_with_stored_layout(layout).unwrap();
+
+                let addr = ptr.addr().get();
+                assert_eq!(addr % align, 0, "alignment {} not respected", align);
+
+                alloc.deallocate_with_stored_layout(ptr.cast());
+            }
+        }
+    }
+
+    #[test]
+    fn reallocate_grow_preserves_data() {
+        unsafe {
+            let alloc = Global;
+
+            let layout1 = Layout::from_size_align(16, 8).unwrap();
+            let layout2 = Layout::from_size_align(64, 8).unwrap();
+
+            let mut ptr = alloc.allocate_with_stored_layout(layout1).unwrap();
+
+            for (index, dest) in ptr.as_mut().iter_mut().enumerate() {
+                *dest = index as u8;
+            }
+
+            let mut ptr = alloc
+                .reallocate_with_stored_layout(ptr.cast(), layout2)
+                .unwrap();
+
+            for (index, dest) in ptr.as_mut().iter_mut().enumerate() {
+                *dest = index as u8;
+            }
+
+            alloc.deallocate_with_stored_layout(ptr.cast());
+        }
+    }
+
+    #[test]
+    fn reallocate_shrink_preserves_data() {
+        unsafe {
+            let alloc = Global;
+
+            let layout1 = Layout::from_size_align(64, 16).unwrap();
+            let layout2 = Layout::from_size_align(16, 16).unwrap();
+
+            let mut ptr = alloc.allocate_with_stored_layout(layout1).unwrap();
+
+            for (index, dest) in ptr.as_mut().iter_mut().enumerate() {
+                *dest = index as u8;
+            }
+
+            let mut ptr = alloc
+                .reallocate_with_stored_layout(ptr.cast(), layout2)
+                .unwrap();
+
+            for (index, dest) in ptr.as_mut().iter_mut().enumerate() {
+                *dest = index as u8;
+            }
+
+            alloc.deallocate_with_stored_layout(ptr.cast());
+        }
+    }
+
+    #[test]
+    fn multiple_allocations_do_not_overlap() {
+        unsafe {
+            let alloc = Global;
+
+            let a = alloc
+                .allocate_with_stored_layout(Layout::from_size_align(32, 8).unwrap())
+                .unwrap();
+            let b = alloc
+                .allocate_with_stored_layout(Layout::from_size_align(32, 8).unwrap())
+                .unwrap();
+
+            let a_addr = a.addr().get();
+            let b_addr = b.addr().get();
+
+            assert_ne!(a_addr, b_addr);
+
+            alloc.deallocate_with_stored_layout(a.cast());
+            alloc.deallocate_with_stored_layout(b.cast());
+        }
+    }
+}
