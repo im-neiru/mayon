f048ac31b44831d72f57554c7bdbb780ff51b6d1
Neil <cantorne.neil@gmail.com>
2026-01-14 21:18:17 +0800

    feat(logging): add macros for logger

--- crates/mayon/src/instance/logger.rs
+++ crates/mayon/src/instance/logger.rs
@@ -68,3 +68,58 @@ impl Logger for QuietLogger {
         /* Be quiet */
     }
 }
+
+#[macro_export]
+macro_rules! error {
+     ($logger:expr, $target:expr, $($arg:tt)*) => {{
+        $logger.log(
+            $crate::logger::Level::Error,
+            $target,
+            core::format_args!($($arg)*),
+        );
+    }};
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($logger:expr, $target:expr, $($arg:tt)*) => {
+          $logger.log(
+            $crate::logger::Level::Warn,
+            $target,
+            core::format_args!($($arg)*),
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! info {
+    ($logger:expr, $target:expr, $($arg:tt)*) => {
+          $logger.log(
+            $crate::logger::Level::Info,
+            $target,
+            core::format_args!($($arg)*),
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($logger:expr, $target:expr, $($arg:tt)*) => {
+          $logger.log(
+            $crate::logger::Level::Debug,
+            $target,
+            core::format_args!($($arg)*),
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! trace {
+    ($logger:expr, $target:expr, $($arg:tt)*) => {
+           $logger.log(
+            $crate::logger::Level::Trace,
+            $target,
+            core::format_args!($($arg)*),
+        );
+    };
+}
