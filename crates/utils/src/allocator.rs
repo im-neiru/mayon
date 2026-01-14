903c08f2f5c0a587465fbcf82a0e6d8d412f9225
Neil <cantorne.neil@gmail.com>
2026-01-14 13:45:19 +0800

    feat(alloc_utils): implement reallocate_with_stored_layout

--- crates/utils/src/allocator.rs
+++ crates/utils/src/allocator.rs
@@ -1,5 +1,6 @@
 use core::{
     alloc::{AllocError, Allocator, Layout},
+    cmp::Ordering::*,
     ptr::NonNull,
 };
 
@@ -48,12 +49,43 @@ where
         ))
     }
 
+    #[allow(unsafe_op_in_unsafe_fn)]
     unsafe fn reallocate_with_stored_layout(
         &self,
         old_ptr: NonNull<u8>,
-        layout: Layout,
+        data_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        todo!()
+        let Ok(new_layout) = LAYOUT_OF_STRUCT_LAYOUT.extend_packed(data_layout) else {
+            return Err(AllocError);
+        };
+
+        let old_layout = {
+            let layout_ptr = old_ptr
+                .as_ptr()
+                .byte_sub(LAYOUT_OF_STRUCT_LAYOUT.size())
+                .cast::<Layout>();
+
+            layout_ptr.read()
+        };
+
+        let ptr = match new_layout.size().cmp(&old_layout.size()) {
+            Greater => self.grow(old_ptr, old_layout, new_layout)?,
+            Less => self.shrink(old_ptr, old_layout, new_layout)?,
+            Equal => {
+                return Ok(NonNull::slice_from_raw_parts(old_ptr, old_layout.size()));
+            }
+        };
+
+        {
+            // store layout
+            let layout_dest = ptr.as_ptr().cast::<Layout>();
+            layout_dest.write(new_layout);
+        }
+
+        Ok(NonNull::slice_from_raw_parts(
+            ptr.byte_add(LAYOUT_OF_STRUCT_LAYOUT.size()).cast(),
+            data_layout.size(),
+        ))
     }
 
     #[allow(unsafe_op_in_unsafe_fn)]
