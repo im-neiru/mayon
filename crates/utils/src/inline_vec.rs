d37aa0e6b4026cb7c817bc737cf724dcf4bc822b
Neil <cantorne.neil@gmail.com>
2026-01-16 18:18:29 +0800

    feat(utils): inline vec constructor and push function

--- crates/utils/src/inline_vec.rs
+++ crates/utils/src/inline_vec.rs
@@ -1,5 +1,23 @@
-#[derive(Clone, Copy, Debug)]
+use core::mem::MaybeUninit;
+
 pub struct InlineVec<T, const CAPACITY: usize> {
-    array: [T; CAPACITY],
+    array: [MaybeUninit<T>; CAPACITY],
     length: usize,
+}
+
+impl<T, const CAPACITY: usize> InlineVec<T, CAPACITY> {
+    #[inline]
+    pub const fn new() -> Self {
+        Self {
+            array: [const { MaybeUninit::uninit() }; CAPACITY],
+            length: 0,
+        }
+    }
+
+    #[inline]
+    pub const fn push(&mut self, value: T) {
+        self.array[self.length] = MaybeUninit::new(value);
+
+        self.length += 1;
+    }
 }
