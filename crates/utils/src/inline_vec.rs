d243f28257c7cfbf0d9989c6ea129bbbcb3f42a5
Neil <cantorne.neil@gmail.com>
2026-01-16 18:24:55 +0800

    feat(utils): implement indexing for InlineVec with Index and IndexMut traits

--- crates/utils/src/inline_vec.rs
+++ crates/utils/src/inline_vec.rs
@@ -1,4 +1,7 @@
-use core::mem::MaybeUninit;
+use core::{
+    mem::MaybeUninit,
+    ops::{Index, IndexMut},
+};
 
 pub struct InlineVec<T, const CAPACITY: usize> {
     array: [MaybeUninit<T>; CAPACITY],
@@ -19,5 +22,48 @@ impl<T, const CAPACITY: usize> InlineVec<T, CAPACITY> {
         self.array[self.length] = MaybeUninit::new(value);
 
         self.length += 1;
+    }
+
+    #[inline]
+    pub const fn length(&self) -> usize {
+        self.length
+    }
+}
+
+impl<T, const CAPACITY: usize> Index<usize> for InlineVec<T, CAPACITY> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        if index >= self.length {
+            panic!(
+                "index out of bounds: the len is {} but the index is {}",
+                self.length, index
+            );
+        }
+
+        unsafe {
+            let element = self.array.get_unchecked(index);
+
+            element.assume_init_ref()
+        }
+    }
+}
+
+impl<T, const CAPACITY: usize> IndexMut<usize> for InlineVec<T, CAPACITY> {
+    #[inline]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if index >= self.length {
+            panic!(
+                "index out of bounds: the len is {} but the index is {}",
+                self.length, index
+            );
+        }
+
+        unsafe {
+            let element = self.array.get_unchecked_mut(index);
+
+            element.assume_init_mut()
+        }
     }
 }
