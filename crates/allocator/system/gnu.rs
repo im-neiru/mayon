71d498ad7a147b4387977515f557221938144b97
Neil <cantorne.neil@gmail.com>
2026-01-18 22:29:11 +0800

    feat(alloc): add padding computation for size storage required by reallocation

--- crates/allocator/system/gnu.rs
+++ crates/allocator/system/gnu.rs
@@ -1,4 +1,4 @@
-use core::ffi::c_void;
+use core::{ffi::c_void, mem, ptr::null_mut};
 
 unsafe extern "C" {
     /// C11 aligned allocation.
@@ -25,18 +25,17 @@ unsafe extern "C" {
     pub(crate) unsafe fn free(ptr: *mut c_void);
 }
 
-#[inline]
-#[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn allocate(size: usize, alignment: usize) -> *mut u8 {
-    if alignment < core::mem::size_of::<usize>() || !alignment.is_power_of_two() {
-        return core::ptr::null_mut();
+    if alignment < mem::size_of::<usize>() || !alignment.is_power_of_two() {
+        return ptr::null_mut();
     }
 
-    let mut ptr: *mut c_void = core::ptr::null_mut();
-    let err = posix_memalign(&mut ptr, alignment, size);
+    let mut ptr: *mut c_void = ptr::null_mut();
+
+    let err = unsafe { posix_memalign(&mut ptr, alignment, size) };
 
     if err != 0 {
-        core::ptr::null_mut()
+        ptr::null_mut()
     } else {
         ptr.cast()
     }
@@ -46,4 +45,49 @@ pub unsafe extern "C" fn allocate(size: usize, alignment: usize) -> *mut u8 {
 #[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn deallocate(ptr: *mut u8) {
     free(ptr.cast())
+}
+
+#[inline]
+#[allow(unsafe_op_in_unsafe_fn)]
+pub unsafe extern "C" fn reallocate(
+    old_ptr: *mut u8,
+    new_size: usize,
+    alignment: usize,
+) -> *mut u8 {
+    let ptr = allocate(new_size, alignment);
+
+    if ptr.is_null() {
+        return null_mut();
+    }
+
+    let old_size; // todo
+
+    ptr.copy_from_nonoverlapping(old_ptr, old_size.min(new_size));
+
+    ptr
+}
+
+struct BlockLayout {
+    data_offset: usize,
+    size: usize,
+    alignment: usize,
+}
+
+impl BlockLayout {
+    const WORD_SIZE: usize = size_of::<usize>();
+
+    fn new(requested_size: usize, requested_alignment: usize) -> Self {
+        let alignment = requested_alignment.max(Self::WORD_SIZE);
+        debug_assert!(alignment.is_power_of_two());
+
+        let data_offset = Self::WORD_SIZE.next_multiple_of(alignment);
+
+        let size = data_offset + requested_size;
+
+        Self {
+            data_offset,
+            size,
+            alignment,
+        }
+    }
 }
