01b618bb1bb41c903f605bb0d6a96fb83ad668e1
Neil <cantorne.neil@gmail.com>
2026-01-18 18:48:05 +0800

    fix(alloc): revise system allocator api

--- crates/allocator/lib.rs
+++ crates/allocator/lib.rs
@@ -4,5 +4,5 @@ mod system;
 pub use allocator::{AllocError, Allocator};
 pub use system::System;
 
-#[cfg(feature = "for_c_api")]
-pub use system::c_api;
+#[cfg(feature = "sys_raw")]
+pub use system::raw;
--- crates/allocator/system/mod.rs
+++ crates/allocator/system/mod.rs
@@ -1,46 +1,47 @@
 #[cfg(miri)]
-mod miri;
+#[path = "windows/mod.rs"]
+pub mod raw;
 
 #[cfg(not(miri))]
 #[cfg(target_family = "unix")]
-mod unix;
+#[path = "windows/mod.rs"]
+pub mod raw;
 
 #[cfg(not(miri))]
 #[cfg(target_family = "windows")]
-mod windows;
+#[path = "windows/mod.rs"]
+pub mod raw;
 
 use core::{alloc::Layout, ptr::NonNull};
 
-use crate::allocator::AllocResult;
+use crate::{AllocError, allocator::AllocResult};
 
 pub struct System;
-
-#[cfg(not(miri))]
-#[cfg(feature = "for_c_api")]
-#[cfg(target_family = "windows")]
-pub use windows::c_api;
-
-#[cfg(not(miri))]
-#[cfg(feature = "for_c_api")]
-#[cfg(target_family = "unix")]
-pub use unix::c_api;
-
-#[cfg(miri)]
-pub use miri::c_api;
 
 unsafe impl crate::Allocator for crate::System {
     #[inline]
     unsafe fn allocate(&self, layout: Layout) -> AllocResult {
-        unsafe { c_api::allocate(layout) }
+        let Some(ptr) = NonNull::new(unsafe { raw::allocate(layout.size(), layout.align()) })
+        else {
+            return Err(AllocError);
+        };
+
+        Ok(NonNull::slice_from_raw_parts(ptr, layout.size()))
     }
 
     #[inline]
     unsafe fn deallocate(&self, ptr: NonNull<u8>) {
-        unsafe { c_api::deallocate(ptr) };
+        unsafe { raw::deallocate(ptr.as_ptr()) };
     }
 
     #[inline]
     unsafe fn reallocate(&self, ptr: NonNull<u8>, new_layout: Layout) -> AllocResult {
-        unsafe { c_api::reallocate(ptr, new_layout) }
+        let Some(ptr) = NonNull::new(unsafe {
+            raw::reallocate(ptr.as_ptr(), new_layout.size(), new_layout.align())
+        }) else {
+            return Err(AllocError);
+        };
+
+        Ok(NonNull::slice_from_raw_parts(ptr, new_layout.size()))
     }
 }
