af79fe954d15de9d03b2dae668455a800d392ad2
Neil <cantorne.neil@gmail.com>
2026-01-20 22:48:06 +0800

    feat(instance): reexport a version of instance with vulkan as backend

--- crates/mayon_vulkan_backend/lib.rs
+++ crates/mayon_vulkan_backend/lib.rs
@@ -9,3 +9,6 @@ pub use errors::{Error, ErrorKind, Result};
 pub use backend::{VulkanBackend, VulkanBackendParams, VulkanVersion};
 pub use context::VulkanContext;
 pub use types::ReturnCode;
+
+pub type Instance<'a, L = mayon_core::logger::DefaultLogger, A = allocator::System> =
+    mayon_core::Instance<VulkanBackend<'a, L, A>, L, A>;
