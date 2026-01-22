817f4e731674f9b29c1ad0f1c4952c82834b614d
Neil <cantorne.neil@gmail.com>
2026-01-22 19:47:56 +0800

    feat(helper-macro): add inner type attribute param in vk_handle marco

--- crates/helper_macros/lib.rs
+++ crates/helper_macros/lib.rs
@@ -5,12 +5,35 @@ use quote::quote;
 use syn::{Fields, ItemStruct, parse_macro_input};
 
 #[proc_macro_attribute]
-pub fn vk_handle(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn vk_handle(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut item_struct = parse_macro_input!(item as ItemStruct);
     let name = &item_struct.ident;
 
+    if attr.is_empty() {
+        panic!("vk_handle attribute requires a type");
+    }
+
+    let raw_type = parse_macro_input!(attr as syn::Ident);
+    let raw_type_str = raw_type.to_string();
+
+    let nonzero_type: syn::Type = match raw_type_str.as_str() {
+        "u8" => syn::parse_quote!(core::num::NonZeroU8),
+        "u16" => syn::parse_quote!(core::num::NonZeroU16),
+        "u32" => syn::parse_quote!(core::num::NonZeroU32),
+        "u64" => syn::parse_quote!(core::num::NonZeroU64),
+        "usize" => syn::parse_quote!(core::num::NonZeroUsize),
+        "i8" => syn::parse_quote!(core::num::NonZeroI8),
+        "i16" => syn::parse_quote!(core::num::NonZeroI16),
+        "i32" => syn::parse_quote!(core::num::NonZeroI32),
+        "i64" => syn::parse_quote!(core::num::NonZeroI64),
+        "isize" => syn::parse_quote!(core::num::NonZeroIsize),
+        _ => {
+            panic!("Unsupported handle type: {raw_type_str}")
+        }
+    };
+
     if let Fields::Unit = &item_struct.fields {
-        item_struct.fields = Fields::Unnamed(syn::parse_quote!((core::num::NonZeroUsize)));
+        item_struct.fields = Fields::Unnamed(syn::parse_quote!((#nonzero_type)));
         item_struct.semi_token = Some(Default::default());
     }
 
@@ -18,6 +41,27 @@ pub fn vk_handle(_attr: TokenStream, item: TokenStream) -> TokenStream {
         #[repr(transparent)]
         #[derive(Copy, Clone, Eq, PartialEq, Hash)]
         #item_struct
+
+        impl #name {
+            #[inline]
+            pub const fn from_raw(raw: #raw_type) -> Option<Self> {
+                let Some(non_zero) = #nonzero_type::new(raw) else {
+                    return None;
+                };
+
+                Some(Self(non_zero))
+            }
+
+            #[inline]
+            pub const unsafe fn from_raw_unchecked(raw: #raw_type) -> Self {
+                unsafe { Self(#nonzero_type::new_unchecked(raw)) }
+            }
+
+            #[inline]
+            pub const fn as_raw(self) -> #raw_type {
+                self.0.get()
+            }
+        }
 
         impl core::fmt::Debug for #name {
             fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
